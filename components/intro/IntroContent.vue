d674b6221eb8ff6e674f5f285dae66696cc1a3cb
tyeon99 <xodus170@naver.com>
2025-04-07 11:10:56 +0900

    하마터면

---
+++ components/intro/IntroContent.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="introContent">
+    <div class="swiper introSwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <div class="title">
+            보이스 피싱,<br />
+            당신은 안전할까요?
+          </div>
+          <div class="txt">철저한 대비만이 우리의 재산을<br />지킵니다.</div>
+        </div>
+        <div class="swiper-slide">
+          <div class="title">하마터면 당할 뻔!</div>
+          <div class="txt">방심은 금물, 지금 바로 대비하세요.</div>
+        </div>
+        <div class="swiper-slide">
+          <div class="title">
+            누구나 속지만,<br />
+            준비된 사람은 다릅니다.
+          </div>
+          <div class="txt">AI 시뮬레이션으로 피싱의 위험에서 <br />벗어나세요.</div>
+        </div>
+      </div>
+      <div class="custom-pgn">
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
+    <div class="start-btn">
+      <button>체험을 시작하세요</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.initializeSwiper()
+  },
+  methods: {
+    initializeSwiper() {
+      this.swiper = new window.Swiper('.introSwiper', {
+        slidesPerView: '1',
+        spaceBetween: 0,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+.introContent{
+  @apply w-full flex flex-col justify-start items-center gap-[68px];
+}
+.introSwiper{
+  @apply w-full h-[592px];
+}
+.swiper-slide{
+  @apply w-full relative px-[24px] pt-[122px] bg-center bg-[length:100%] bg-no-repeat bg-fixed flex flex-col items-start gap-[20px];
+}
+.swiper-slide:nth-child(1){
+  @apply bg-[url(~/assets/img/intro/intro-bg01.png)] justify-start;
+}
+.swiper-slide:nth-child(2){
+  @apply bg-[url(~/assets/img/intro/intro-bg02.png)] justify-center pb-[40px];
+}
+.swiper-slide:nth-child(3){
+  @apply bg-[url(~/assets/img/intro/intro-bg03.png)] justify-center pb-[92px];
+}
+.swiper-slide .title{
+  @apply font-[JalnanGothic] text-[#fff] text-[32px] font-normal leading-[42px] tracking-[-2px];
+  text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.40);
+  font-feature-settings: 'liga' off, 'clig' off;
+}
+.swiper-slide .txt{
+  @apply text-[#fff] text-[20px] font-medium leading-[24px] tracking-[-0.4px];
+  text-shadow: 0px 1px 2.8px rgba(0, 0, 0, 0.40);
+}
+.custom-pgn{
+  @apply w-full flex justify-center items-center;
+}
+.swiper-pagination{
+  @apply w-auto p-[6px_14px];
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: '하마터면',
+    htmlAttrs: {
+      lang: 'ko',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      {
+        rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css'
+      }
+    ],
+    script: [
+      {
+        src: 'https://fastly.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+        defer: true
+      }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/common.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  router: {
+    base: '/hama/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  }
+}
